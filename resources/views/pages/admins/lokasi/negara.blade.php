8978b92cf5864f9ee474719b9109777d79ecabbe
m1ku100 <ilham.puji100@gmail.com>
2020-03-02 11:06:49 +0700

    Set Lokasi Page

---
+++ resources/views/pages/admins/lokasi/negara.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.mst_admin')
+@section('title', 'Admin '.env('APP_NAME').': Kelola Data Negara | '.env('APP_TITLE'))
+
+@push('styles')
+    <style></style>
+@endpush
+
+@section('content')
+@endsection
+
+@push('scripts')
+    <script></script>
+@endpush
