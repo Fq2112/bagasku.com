0aaa31c0dee8883866d2f4d78809dd944efb79d2
m1ku100 <ilham.puji100@gmail.com>
2020-03-26 21:51:02 +0700

    Add export in Pembayaran proyek layanan

--- resources/views/pages/admins/pembayaran/service.blade.php
+++ resources/views/pages/admins/pembayaran/service.blade.php
@@ -111,7 +111,60 @@
     <script src="{{asset('admins/js/page/modules-datatables.js')}}"></script>
     <script !src="">
 
+
+        var export_pesanan = 'Daftar Pembayaran Layanan pada tanggal ({{now()->format('j F Y')}})';
         $("#dt-service").DataTable({
+            dom: "<'row'<'col-sm-12 col-md-3'l><'col-sm-12 col-md-5'B><'col-sm-12 col-md-4'f>>" +
+                "<'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+            language: {
+                "emptyTable": "Anda belum memesan layanan apapun",
+                "info": "Menampilkan _START_ to _END_ of _TOTAL_ entri",
+                "infoEmpty": "Menampilkan 0 entri",
+                "infoFiltered": "(difilter dari _MAX_ total entri)",
+                "lengthMenu": "Tampilkan _MENU_ entri",
+                "loadingRecords": "Memuat...",
+                "processing": "Mengolah...",
+                "search": "Cari:",
+                "zeroRecords": "Data yang Anda cari tidak ditemukan.",
+                "paginate": {
+                    "first": "Pertama",
+                    "last": "Terakhir",
+                    "next": "Selanjutnya",
+                    "previous": "Sebelumnya"
+                },
+                "aria": {
+                    "sortAscending": ": aktifkan untuk mengurutkan kolom dari kecil ke besar (asc)",
+                    "sortDescending": ": aktifkan untuk mengurutkan kolom dari besar ke kecil (desc)"
+                }
+            },
+            buttons: [
+                {
+                    text: '<b class="text-uppercase"><i class="far fa-file-excel mr-2"></i>Excel</b>',
+                    extend: 'excel',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                    },
+                    className: 'btn btn-primary',
+                    title: export_pesanan,
+                    extension: '.xls'
+                }, {
+                    text: '<b class="text-uppercase"><i class="fa fa-file-pdf mr-2"></i>PDF</b>',
+                    extend: 'pdf',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                    },
+                    className: 'btn btn-primary',
+                    title: export_pesanan,
+                    extension: '.pdf'
+                }, {
+                    text: '<b class="text-uppercase"><i class="fa fa-print mr-2"></i>Cetak</b>',
+                    extend: 'print',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                    },
+                    className: 'btn btn-primary'
+                }
+            ],
             fnDrawCallback: function (oSettings) {
                 $('.use-nicescroll').getNiceScroll().resize();
                 $('[data-toggle="tooltip"]').tooltip();
